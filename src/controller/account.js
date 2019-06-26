7c5088c39fd5e2329ab8a51e94452d40ab334672
marvin evins <maevins@cps.edu>
2019-06-26 14:54:45 -0500

    fixed errors

--- src/controller/account.js
+++ src/controller/account.js
@@ -11,5 +11,41 @@ import { generateAccessToken, respond, authenticate} from '../middleware/authmid
 export default ({config, db})=>{
   let api = Router();
 
+  // '/v1/account'
+  api.post('/register', (req, res) => {
+    Account.register(new Account({
+      username: req.body.email
+    }),  req.body.password, function(err, account){
+       if(err){
+         res.send(err);
+       }
+       passport.authenticate(
+         'local', {
+           session: false
+         })(req, res, ()=> {
+           res.status(200).send('successfully created new account');
+         });
+
+    });
+  });
+
+  // '/v1/account/login'
+  api.post('/login', passport.authenticate(
+      'local', {
+        session: false,
+        scope: []
+      }), generateAccessToken, respond);
+
+
+
+  api.get('/logout', authenticate,(req, res)=> {
+    res.logout();
+    res.status(200).send('successufully logged out');
+  });
+
+  api.get('/me', authenticate, (req, res)=>{
+    res.status(200).json(req.user);
+  });
+
   return api;
 }
